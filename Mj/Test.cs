46b3ae0f74403e777bdd628410d11fbbb1a49d7d
Егор Тарабан <egor.taraban@gmail.com>
2020-02-06 22:42:11 +0200

    Set Up test 1

---
+++ Mj/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace Mj
+{
+    public class Test
+    {
+        [Test]
+        public void TestRun()
+        {
+            ChromeDriver chrome = new ChromeDriver();
+            chrome.Navigate().GoToUrl("https://metanit.com");
+            chrome.Quit();
+        }
+    }
+}
